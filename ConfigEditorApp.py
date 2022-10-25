78d15f87b53d070bca91f7b743dd3c311f5f7928
krajos <krososmail@gmail.com>
2022-10-25 20:23:59 +0200

    ubuntu-fix

--- ConfigEditorApp.py
+++ ConfigEditorApp.py
@@ -3,21 +3,23 @@ from kivy.config import Config
 Config.set('graphics', 'width', 1200)
 Config.set('graphics', 'height', 600)
 Config.set('graphics', 'resizable', 0)
-#Window.clearcolor=(46/255,45/255,45/255,0.5)
+# Window.clearcolor=(46/255,45/255,45/255,0.5)
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
-from Screens import DummyScreen
-from Screens import MainScreen
+from kivy.lang import Builder
+from Screens.DummyScreen import DummyScreen
+from Screens.MainScreen import MainScreen
+
+Builder.load_file("ConfigEditorApp.kv")
+screen_manager = ScreenManager()
+screen_manager.add_widget(DummyScreen())
+screen_manager.add_widget(MainScreen())
 
 
 class ConfigEditorApp(App):
     def build(self):
-        pass
-
-
-class Manager(ScreenManager):
-    pass
+        return screen_manager
 
 
 if __name__ == '__main__':
